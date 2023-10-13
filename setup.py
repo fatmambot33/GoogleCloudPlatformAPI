61b8f7904049318af6f6c8d8104221747f772356
fatmambo33 <fatmambo33@gmail.com>
2023-10-13 08:53:47 +0200

    new file:   pyproject.toml 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GoogleCloudPlatformAPI',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "google-api-python-client",
+        "google-cloud-bigquery",
+        "google-cloud-storage"
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
