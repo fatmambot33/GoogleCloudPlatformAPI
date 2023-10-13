ea3c9fd4796c3d5484006edf936a025dcffd76b3
fatmambo33 <fatmambo33@gmail.com>
2023-10-13 12:38:11 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GoogleCloudPlatformAPI',
-    version='1.12',
+    version='v2.0.1',
     packages=find_packages(),
     install_requires=[
 
