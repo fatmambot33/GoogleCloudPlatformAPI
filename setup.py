e3ab92d2779e14e08734ec1b8d917876960ba5eb
fatmambo33 <fatmambo33@gmail.com>
2023-10-13 10:16:49 +0200

    deleted:    GoogleCloudPlatform/__init__.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GoogleCloudPlatformAPI',
-    version='1.11',
+    version='1.12',
     packages=find_packages(),
     install_requires=[
         "pandas",
