a889e5039383c75ea01dc0077d2e554d38af79fe
fatmambo33 <fatmambo33@gmail.com>
2023-11-05 16:41:17 +0100

    modified:   GoogleCloudPlatformAPI/__init__.py 	modified:   requirements.txt 	modified:   setup.py

--- GoogleCloudPlatformAPI/__init__.py
+++ GoogleCloudPlatformAPI/__init__.py
@@ -1,4 +1,9 @@
-GAM_VERSION = "v202305"
-PYTZ_TIMEZONE = "GMT"
-NETWORK_CODE = '5574'
-APP_NAME = 'AdManager'
+from .AdManager import Audience,Network,Report,TargetingPreset,Traffic,Forecast
+from .Analytics import Analytics
+from .BigQuery import BigQuery
+from .CloudStorage import CloudStorage
+
+__all__ = ["Audience","Network","Report","TargetingPreset","Traffic","Forecast",
+           "Analytics",
+           "BigQuery",
+           "CloudStorage"]
