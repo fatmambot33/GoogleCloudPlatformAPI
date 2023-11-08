ae28783590cfc312fd4ecf7b973ac8e5f917a9dd
fatmambo33 <fatmambo33@gmail.com>
2023-11-08 23:32:28 +0100

    new file:   GoogleCloudPlatformAPI.egg-info/PKG-INFO 	new file:   GoogleCloudPlatformAPI.egg-info/SOURCES.txt 	new file:   GoogleCloudPlatformAPI.egg-info/dependency_links.txt 	new file:   GoogleCloudPlatformAPI.egg-info/requires.txt 	new file:   GoogleCloudPlatformAPI.egg-info/top_level.txt	modified:   GoogleCloudPlatformAPI/__init__.py

--- GoogleCloudPlatformAPI/__init__.py
+++ GoogleCloudPlatformAPI/__init__.py
@@ -1,7 +1,12 @@
+
 from .AdManager import Audience,Network,Report,TargetingPreset,Traffic,Forecast
 from .Analytics import Analytics
 from .BigQuery import BigQuery
 from .CloudStorage import CloudStorage
+import requirements
+
+requirements.main()
+
 
 __all__ = ["Audience","Network","Report","TargetingPreset","Traffic","Forecast",
            "Analytics",
