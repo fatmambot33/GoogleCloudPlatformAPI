6e85a1e71b949558d2af77582e2d1667842cc6fc
fatmambo33 <fatmambo33@gmail.com>
2024-02-15 12:58:15 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if os.path.exists(requirements_file):
 
 setup(
     name='GoogleCloudPlatformAPI',
-    version='v2.2.2',
+    version='v2.2.3',
     packages=find_packages(),
     install_requires=requirements,
     long_description=open('README.md').read(),
